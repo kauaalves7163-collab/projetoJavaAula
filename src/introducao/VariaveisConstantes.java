4607a3ff2a2cbf0fca631aeaac762f1640e4044f
kaua alves <kauaalves7163@icloud.com>
2026-04-17 10:53:59 -0300

    novo projeto

---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,19 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//variaveis
+		int idade = 25;
+		String nome = "kaua";
+		float graus = 20.0f;
+		boolean resposta;
+		
+		System.out.println(nome);
+		System.out.println(graus);
+		
+	}
+
+}
